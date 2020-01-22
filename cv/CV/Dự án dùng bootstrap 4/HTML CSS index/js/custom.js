29033107eb4b982fc413864dd4ed305085163f16
HungNguyen.IT312 <namdaica8000@gmail.com>
2020-01-22 14:12:21 +0700

    cv intership front-end

---
+++ cv/CV/Dự án dùng bootstrap 4/HTML CSS index/js/custom.js
@@ -0,0 +1,18 @@
+$("a[href^='#']").click(function(e) {
+	e.preventDefault();
+	
+	var position = $($(this).attr("href")).offset().top;
+
+	$("body, html").animate({
+		scrollTop: position
+	} /* speed */ );
+});
+var header = document.getElementById("myDIV");
+var btns = header.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+  var current = document.getElementsByClassName("active");
+  current[0].className = current[0].className.replace(" active", "");
+  this.className += " active";
+  });
+}
